b203b30ab1da2112368a7747dd18684cf0c4cc63
Douglas Fugita <douglas.cezar@gmail.com>
2021-05-29 11:34:20 -0300

    Basic Demo

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# net5_rabbitmq_lab
+# RabbitMQ Lab
+
+## Docker
+```
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+```
+## Basic Demo
+Based in RabbitMQ Tutorial: https://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+
+First Terminal:
+```
+dotnet run --project basic_demo/Receive/
+```
+Second Terminal:
+```
+dotnet run --project basic_demo/Send/
+```
+
+
+## Resources:
+- https://www.rabbitmq.com/
---
+++ basic_demo/Send/Send.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main(string[] args)
+        {
+            var queueName = "demo";
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: queueName,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var totalMsg = 10;
+
+                for (var i = 0; i < totalMsg; i++)
+                {
+                    string message = $"Hello Basic Demo! Message: {i+1}/{totalMsg}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: queueName,
+                        basicProperties: null,
+                        body: body
+                        );
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+
+            }
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
